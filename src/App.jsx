a5ac558e2f6445549706679c50d255107419c87f
Ben <66370595+unhappyben@users.noreply.github.com>
2025-04-11 15:46:21 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { useState, useRef } from 'react'
 import { toPng } from 'html-to-image'
 import './index.css'
-import catHappy from './assets/cat-happy.png'
-import catSad from './assets/cat-sad.png'
+//import catHappy from './assets/cat-happy.png'
+//import catSad from './assets/cat-sad.png'
 
 
 const COMPARISON_TOKENS = ['bitcoin', 'ethereum', 'solana', 'fartcoin']
@@ -186,9 +186,12 @@ export default function App() {
               <img
                 src={
                   parseFloat(result[normalizedToken]) >= parseFloat(amountUSD)
-                    ? catHappy
-                    : catSad
+                    ? 'https://i.imgur.com/cJ2JHC5.png' // happy
+                    : 'https://i.imgur.com/hROKQ0K.png' // sad
                 }
+                alt="Cat Mood"
+                className="w-full"
+                crossOrigin="anonymous"
               />
               </div>
             </div>
